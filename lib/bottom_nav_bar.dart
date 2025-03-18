136b57fe757e2184e06be61da6c6def51e07eb56
Whitney Haddad <whaddad@msudenver.edu>
2025-03-18 17:35:05 -0600

    Improve Product Purchasing Guide UI to be cohesive with the Figma design also ensured  responsive design and fixed navigation bar to be cohesive with Figma design as well as added navigation logic for Home button

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -1,45 +1,49 @@
 import 'package:flutter/material.dart';
+import 'main_page.dart';
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: Colors.white,
-      shape: const CircularNotchedRectangle(), // Optional rounded effect
-      notchMargin: 5, // Space around the notch
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.home, color: Colors.black),
-              onPressed: () {
-                // TODO: Implement navigation later
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.search, color: Colors.black),
-              onPressed: () {
-                // TODO: Implement navigation later
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.favorite, color: Colors.black),
-              onPressed: () {
-                // TODO: Implement navigation later
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.chat, color: Colors.black),
-              onPressed: () {
-                // TODO: Implement navigation later
-              },
-            ),
-          ],
-        ),
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.home, color: Colors.black, size: 30),
+            onPressed: () {
+              // Navigate to home page
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MainPage()),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.black, size: 30),
+            onPressed: () {
+              // Search functionality to be implemented
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.favorite, color: Colors.black, size: 30),
+            onPressed: () {
+              // Favorites functionality to be implemented
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.chat_bubble, color: Colors.black, size: 30),
+            onPressed: () {
+              // Chat functionality to be implemented
+            },
+          ),
+        ],
       ),
     );
   }
