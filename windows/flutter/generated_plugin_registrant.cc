11ed35d807cde92be80d916b18291e7d335d64f3
zaidalhamdan <143560116+zaidalhamdan@users.noreply.github.com>
2025-04-24 10:29:30 -0600

    Revert "Zaidembed"

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,16 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-
-#include <flutter_inappwebview_windows/flutter_inappwebview_windows_plugin_c_api.h>
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-
 }
